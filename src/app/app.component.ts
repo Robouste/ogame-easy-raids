c64bf91c2e7765d105e8a3fc2c1f188e1a29bee3
Robouste <robindenis88@gmail.com>
2020-08-02 15:55:45 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,169 @@
+import { Component, OnInit, ViewChild, AfterViewInit } from "@angular/core";
+import { trigger, state, style, transition, animate } from "@angular/animations";
+import { MatTableDataSource } from "@angular/material/table";
+import { MatSort, MatSortable } from "@angular/material/sort";
+import { MatSnackBar } from "@angular/material/snack-bar";
+
+@Component({
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"],
+	animations: [
+		trigger("slide", [
+			state("closed", style({ height: "0px", minHeight: "0" })),
+			state("opened", style({ height: "*" })),
+			transition("opened <=> closed", animate("225ms cubic-bezier(0.4, 0.0, 0.2, 1)")),
+		]),
+	],
+})
+export class AppComponent implements AfterViewInit {
+	public state: string = "opened";
+	public rawData: string;
+	public spyReports: SpyReport[] = [];
+	public displayedColumns: string[] = ["index", "resources", "flottes", "defenses", "noCargo", "coordinates"];
+	public dataSource: MatTableDataSource<SpyReport> = new MatTableDataSource(this.spyReports);
+	public cargoCapacity: number = 41250;
+	public lastElementClicked: SpyReport;
+
+	@ViewChild(MatSort, { static: false }) sort: MatSort;
+
+	constructor(private snackbar: MatSnackBar) {}
+
+	ngAfterViewInit(): void {
+		this.dataSource.sort = this.sort;
+	}
+
+	process(clean: boolean): void {
+		if (clean) {
+			this.spyReports = [];
+		}
+
+		const splitRegex = new RegExp("Rapport d`((?:.|\n)*)Plus", "gm");
+		const reports = this.rawData.match(splitRegex)[0].split("Plus de détails");
+
+		for (const r of reports) {
+			this.parseReport(r);
+		}
+
+		const coords = this.spyReports.map((rep: SpyReport) => rep.getCoordinates());
+		const duplicatesCoord = coords.filter((coord: string, index: number) => coords.indexOf(coord) !== index);
+
+		if (duplicatesCoord.length) {
+			this.snackbar.open(`found duplicate(s) ${duplicatesCoord.join(",")}`);
+			const duplicates: SpyReport[] = [];
+
+			this.spyReports = this.spyReports.filter((rep: SpyReport) => {
+				if (
+					duplicatesCoord.includes(rep.getCoordinates()) &&
+					!duplicates.some((dup: SpyReport) => dup.getCoordinates() === rep.getCoordinates())
+				) {
+					duplicates.push(rep);
+					return false;
+				}
+				return true;
+			});
+		}
+
+		this.dataSource.data = this.spyReports;
+
+		if (!this.dataSource.sort.active) {
+			const sortable: MatSortable = {
+				id: "resources",
+				start: "desc",
+				disableClear: false,
+			};
+			this.dataSource.sort.sort(sortable);
+		}
+
+		this.state = "closed";
+	}
+
+	parseReport(report: string): void {
+		const result = new SpyReport();
+		const resourceRegEx: RegExp = new RegExp("Ressources: (.*)");
+		const coordRegex: RegExp = new RegExp(/(\[(.*)\])/);
+		const fleetRegex: RegExp = new RegExp("Flottes: (.*)D");
+		const defenseRegex: RegExp = new RegExp("Défense: (.*)");
+
+		result.setRessources(resourceRegEx.exec(report)[1], this.cargoCapacity);
+		result.setCoordinates(coordRegex.exec(report)[2]);
+		const fleets = fleetRegex.exec(report);
+		result.flottes = fleets ? fleetRegex.exec(report)[1] : "??";
+		const defense = defenseRegex.exec(report);
+		result.defenses = defense ? defenseRegex.exec(report)[1] : "??";
+
+		this.spyReports.push(result);
+	}
+
+	resetFilter(): void {
+		this.dataSource.data = this.spyReports;
+	}
+
+	clearDefense(): void {
+		this.dataSource.data = this.spyReports.slice().filter((rep: SpyReport) => rep.defenses === "0");
+	}
+
+	clearFleet(): void {
+		this.dataSource.data = this.spyReports.slice().filter((rep: SpyReport) => rep.flottes === "0");
+	}
+
+	clearBoth(): void {
+		this.dataSource.data = this.spyReports
+			.slice()
+			.filter((rep: SpyReport) => rep.flottes === "0" && rep.defenses === "0");
+	}
+
+	navigate(report: SpyReport): void {
+		this.lastElementClicked = report;
+		window.open(this.getLink(report.coordinates, report.noCargo), "_blank");
+	}
+
+	getLink(coordinate: Coordinate, cargo: number): string {
+		return `https://s172-fr.ogame.gameforge.com/game/index.php?page=ingame&component=fleetdispatch&galaxy=${coordinate.galaxy}&system=${coordinate.system}&position=${coordinate.position}&type=1&mission=1&cargo=${cargo}`;
+	}
+}
+
+export class SpyReport {
+	public resources: number;
+	public flottes: string;
+	public defenses: string;
+	public noCargo: number;
+	public coordinates: Coordinate;
+
+	setRessources(rawResource: string, capacity: number): void {
+		rawResource = rawResource.replace(",", "");
+		rawResource = rawResource.replace(".", "");
+
+		if (rawResource.includes("M")) {
+			rawResource = rawResource.slice(0, -1);
+			this.resources = parseFloat(rawResource) * 1000;
+		} else {
+			this.resources = parseFloat(rawResource);
+		}
+
+		this.noCargo = Math.ceil(this.resources / 2 / capacity);
+	}
+
+	setCoordinates(coord: string): void {
+		const group = coord.split(":");
+		const coords = new Coordinate();
+		coords.galaxy = +group[0];
+		coords.system = +group[1];
+		coords.position = +group[2];
+		this.coordinates = coords;
+	}
+
+	getCoordinates(): string {
+		return this.coordinates.toString();
+	}
+}
+
+export class Coordinate {
+	public galaxy: number;
+	public system: number;
+	public position: number;
+
+	toString(): string {
+		return `[${this.galaxy}:${this.system}:${this.position}]`;
+	}
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatTableModule } from "@angular/material/table";
+import { MatIconModule } from "@angular/material/icon";
+import { MatRippleModule } from "@angular/material/core";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatSortModule } from "@angular/material/sort";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
+@NgModule({
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		CommonModule,
+		FormsModule,
+		MatCardModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatButtonModule,
+		MatTableModule,
+		MatIconModule,
+		MatRippleModule,
+		MatPaginatorModule,
+		MatSortModule,
+		MatSnackBarModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
+})
+export class AppModule {}
